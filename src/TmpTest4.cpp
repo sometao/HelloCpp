50d763f78b2f858f4db82b7d0f4716743784eaea
Tao Zhang <zta@outlook.com>
2020-03-22 22:32:12 +0800

    test unique_ptr

--- src/TmpTest4.cpp
+++ src/TmpTest4.cpp
@@ -1,6 +1,8 @@
 
 #include <fstream>
 #include <iostream>
+#include <list>
+#include <memory>
 #include <unordered_map>
 
 #include "EasyWay.h"
@@ -76,13 +78,14 @@ uint32_t arrToHashCode(int arr[10]) {
   return code;
 }
 
+namespace solution1 {
+
 int recursiveCount = 0;
 int returnCount = 0;
 
 std::unordered_map<uint32_t, int> stateMap = {};
 
 int calcStep(int poker[10]) {
-
   auto code = arrToHashCode(poker);
   auto t = stateMap.find(code);
   if (t != stateMap.end()) {
@@ -96,11 +99,11 @@ int calcStep(int poker[10]) {
 
   recursiveCount++;
 
-  if (recursiveCount % 10000 == 0) {
-    cout << "recursiveCount     =    " << recursiveCount << endl;
-    cout << "returnCount     =    " << returnCount << endl;
-    printArray10(poker);
-  }
+  // if (recursiveCount % 10000 == 0) {
+  //  cout << "recursiveCount     =    " << recursiveCount << endl;
+  //  cout << "returnCount     =    " << returnCount << endl;
+  //  printArray10(poker);
+  //}
 
   // cout << " 0 +++++++++++++++++++++++++++++++++++++++++++++++++++++ " <<
   // endl; cout << " -- poker -- " << endl; printArray10(poker);
@@ -229,35 +232,16 @@ int calcStep(int poker[10]) {
     return danzhangCount;
   } else {
     int min_count = INT_MAX;
-    // send a shunzi out
+
+    // send a duizi out
     for (int i = 0; i < 10 && min_count > 0; i++) {
-      if (shunzi[i] == 1) {
-        int nextPoker[10] = {0};
+      if (duizi[i] == 1) {
+        int nextPoker[10];
         memcpy(nextPoker, poker, 10 * sizeof(int));
-
-        // cout << " -- poker -- " << endl;
-        // printArray10(poker);
-        // cout << " --init:  nextPoker -- " << endl;
-        // printArray10(nextPoker);
-
-        for (int j = i; j < i + 5; j++) {
-          // cout << "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-          // -- " << endl; cout << " -- poker -- " << endl; printArray10(poker);
-
-          // cout << " -- shunzi -- " << endl;
-          // printArray10(shunzi);
-
-          // cout << " i = " << i << endl;
-          // cout << " j = " << j << endl;
-
-          --nextPoker[j];
-
-          // cout << " -- nextPoker -- " << endl;
-          // printArray10(nextPoker);
-
-          if (nextPoker[j] < 0) {
-            throw std::runtime_error("nextPoker[j] < 0 in send a shunzi out");
-          }
+        nextPoker[i] -= 2;
+        if (nextPoker[i] < 0) {
+          cout << "nextPoker[j] < 0 in send a liandui out" << endl;
+          throw std::runtime_error("nextPoker[j] < 0 in send a liandui out");
         }
 
         int nextPokerCount = 0;
@@ -324,15 +308,35 @@ int calcStep(int poker[10]) {
       }
     }
 
-    // send a duizi out
+    // send a shunzi out
     for (int i = 0; i < 10 && min_count > 0; i++) {
-      if (duizi[i] == 1) {
-        int nextPoker[10];
+      if (shunzi[i] == 1) {
+        int nextPoker[10] = {0};
         memcpy(nextPoker, poker, 10 * sizeof(int));
-        nextPoker[i] -= 2;
-        if (nextPoker[i] < 0) {
-          cout << "nextPoker[j] < 0 in send a liandui out" << endl;
-          throw std::runtime_error("nextPoker[j] < 0 in send a liandui out");
+
+        // cout << " -- poker -- " << endl;
+        // printArray10(poker);
+        // cout << " --init:  nextPoker -- " << endl;
+        // printArray10(nextPoker);
+
+        for (int j = i; j < i + 5; j++) {
+          // cout << "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+          // -- " << endl; cout << " -- poker -- " << endl; printArray10(poker);
+
+          // cout << " -- shunzi -- " << endl;
+          // printArray10(shunzi);
+
+          // cout << " i = " << i << endl;
+          // cout << " j = " << j << endl;
+
+          --nextPoker[j];
+
+          // cout << " -- nextPoker -- " << endl;
+          // printArray10(nextPoker);
+
+          if (nextPoker[j] < 0) {
+            throw std::runtime_error("nextPoker[j] < 0 in send a shunzi out");
+          }
         }
 
         int nextPokerCount = 0;
@@ -356,58 +360,329 @@ int calcStep(int poker[10]) {
   }
 }
 
-int calcCount(int* input, int length) {
-  int poker[10] = {0};
-  // TODO
-  for (int i = 0; i < length; i++) {
-    poker[input[i] - 1]++;
-  }
-
+int calcCount(int poker[10]) {
   int rst = calcStep(poker);
 
   return rst;
 }
 
+}  // namespace solution1
+
 void tmp03() {
-  int input[] = {1, 2, 7, 8, 9, 10,
-                  1, 2, 3, 3, 4, 4, 5, 5, 6, 7, 8, 9, 10,
-                  1, 2, 3, 3, 4, 4, 5, 5, 6, 7, 8, 9, 10,
+  using namespace solution1;
+
+  int input[] = {
+      1, 2, 7,  8, 9, 10, 1, 2, 3, 4, 4, 5, 5, 6, 7,
+      8, 9, 10, 1, 2, 3,  4, 4, 5, 5, 6, 7, 8, 9, 10,
   };
 
-  //int input[] = {1, 2, 3, 3, 4, 4, 5, 5, };
+  // int input[] = {1, 2, 3, 3, 4, 4, 5, 5, };
 
-   int len = sizeof(input) / sizeof(int);
+  int len = sizeof(input) / sizeof(int);
 
-   cout << "len:" << len << endl;
+  cout << "len:" << len << endl;
 
-   int rst = calcCount(input, len);
+  int poker[10] = {0};
 
-   cout << "recursiveCount:" << recursiveCount << endl;
-   cout << "rst:" << rst << endl;
+  for (int i = 0; i < len; i++) {
+    poker[input[i] - 1]++;
+  }
 
+  cout << " -- poker -- " << endl;
+  printArray10(poker);
+
+  auto t0 = EasyWay::currentTimeMillis();
+
+  int rst = calcCount(poker);
+  auto t1 = EasyWay::currentTimeMillis();
+
+  cout << "recursiveCount:" << recursiveCount << endl;
+  cout << "rst:" << rst << endl;
+  cout << "time:" << (t1 - t0) << "ms" << endl;
 }
 
 void tmp04() {
-
   uint64_t a = 1;
   cout << "a: " << a << endl;
 
-
-  a <<= 2;
-  cout << "a: " << a << endl;
-  
-  a <<= 2;
-  cout << "a: " << a << endl;
-  
   a <<= 2;
   cout << "a: " << a << endl;
 
+  a <<= 2;
+  cout << "a: " << a << endl;
 
+  a <<= 2;
+  cout << "a: " << a << endl;
+}
+
+namespace solution2 {
+
+int recursiveCount = 0;
+
+bool finish(int poker[10]) {
+  for (int i = 0; i < 10; i++) {
+    if (poker[i] > 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+int min = INT_MAX;
+
+void calcStep(int poker[10], int count) {
+  recursiveCount++;
+  if (count >= min) {
+    return;
+  }
+
+  if (finish(poker)) {
+    if (count < min) {
+      min = count;
+    }
+    return;
+  }
+
+  int i = 0;
+  while (poker[i] == 0) {
+    i++;
+  }
+
+  //五连
+  if (i + 5 <= 10 && poker[i] > 0 && poker[i + 1] > 0 && poker[i + 2] > 0 &&
+      poker[i + 3] > 0 && poker[i + 4] > 0) {
+    for (int j = 0; j < 5; j++) {
+      poker[i + j]--;
+    }
+    calcStep(poker, count + 1);
+    for (int j = 0; j < 5; j++) {
+      poker[i + j]++;
+    }
+  }
+
+  //三双
+  if (i + 3 <= 10 && poker[i] > 1 && poker[i + 1] > 1 && poker[i + 2] > 1) {
+    for (int j = 0; j < 3; j++) {
+      poker[i + j] -= 2;
+    }
+    calcStep(poker, count + 1);
+    for (int j = 0; j < 3; j++) {
+      poker[i + j] += 2;
+    }
+  }
+
+  //对子
+  if (poker[i] > 1) {
+    poker[i] -= 2;
+    calcStep(poker, count + 1);
+    poker[i] += 2;
+  }
+
+  //单子
+  if (poker[i] > 0) {
+    poker[i] -= 1;
+    calcStep(poker, count + 1);
+    poker[i] += 1;
+  }
+}
+
+}  // namespace solution2
+
+void tmp05() {
+  using namespace solution2;
+
+  int poker[] = {3, 3, 2, 4, 4, 2, 3, 3, 3, 3};
+
+  cout << " -- poker -- " << endl;
+  printArray10(poker);
+
+  auto t0 = EasyWay::currentTimeMillis();
+
+  calcStep(poker, 0);
+  auto t1 = EasyWay::currentTimeMillis();
+
+  cout << "recursiveCount:" << recursiveCount << endl;
+  cout << "rst:" << min << endl;
+  cout << "time:" << (t1 - t0) << "ms" << endl;
+}
+
+class Bar06 {
+  string name;
+
+ public:
+  Bar06(const string& n) : name(n) {
+    cout << "create Bar06[" << name << "]" << endl;
+  }
+
+  Bar06(const Bar06& o) : name(o.name) {
+    cout << "copy Bar06[" << name << "]" << endl;
+  }
+
+  Bar06(Bar06&& o) noexcept : name(o.name) {
+    o.name = "NULL";
+    cout << "move Bar06[" << name << "]" << endl;
+  }
+
+  ~Bar06() { cout << "delete Bar06[" << name << "]" << endl; }
+};
+
+class BarDeleter06 {
+ public:
+  string name = "NULL";
+  BarDeleter06() { cout << "create BarDeleter06[" << name << "]" << endl; }
+
+  BarDeleter06(const BarDeleter06& o) {
+    name = o.name;
+    cout << "copy BarDeleter06[" << name << "]" << endl;
+  }
+
+  BarDeleter06(BarDeleter06&& o) noexcept {
+    name = o.name;
+    cout << "move BarDeleter06[" << name << "]" << endl;
+  }
+
+  BarDeleter06& operator=(const BarDeleter06& o) {
+    cout << "operator= set[" << name << "] to [" << o.name << "]" << endl;
+    name = o.name;
+    return *this;
+  };
+
+  void operator()(Bar06* p) const {
+    cout << "- BarDeleter06 delete begin[" << name << "]" << endl;
+    p->~Bar06();
+    cout << "- BarDeleter06 delete end[" << name << "]" << endl;
+  };
+};
+
+void tmp06a() {
+  {
+    cout << "------------  1  ----------------" << endl;
+    std::unique_ptr<Bar06> p1 = std::make_unique<Bar06>("aa");
+    cout << "------------  2  ----------------" << endl;
+  }
+  cout << "------------  3  ----------------" << endl;
+
+  std::unique_ptr<Bar06> p2;
+  {
+    cout << "------------  4  ----------------" << endl;
+    std::unique_ptr<Bar06> p3 = std::make_unique<Bar06>("bb");
+    cout << "------------  5  ----------------" << endl;
+    p2.reset(p3.release());
+    cout << "------------  6  ----------------" << endl;
+  }
+  cout << "------------  7  ----------------" << endl;
+
+  cout << "done." << endl;
+}
+
+void tmp06b() {
+  BarDeleter06 d{};
+
+  {
+    cout << "------------  1  ----------------" << endl;
+    std::unique_ptr<Bar06, BarDeleter06&> p{new Bar06("aa"), d};
+    cout << "------------  2  ----------------" << endl;
+  }
+  cout << "------------  3  ----------------" << endl;
+
+  std::unique_ptr<Bar06, BarDeleter06> p2{new Bar06("xx"), BarDeleter06()};
+  {
+    cout << "------------  4  ----------------" << endl;
+    std::unique_ptr<Bar06, BarDeleter06> p{new Bar06("bb"), BarDeleter06()};
+    cout << "------------  5  ----------------" << endl;
+    p2.reset(p.release());
+    cout << "------------  6  ----------------" << endl;
+  }
+  cout << "------------  7  ----------------" << endl;
+
+  std::unique_ptr<Bar06, BarDeleter06> p3{};
+  {
+    cout << "------------  8  ----------------" << endl;
+    std::unique_ptr<Bar06, BarDeleter06> p{new Bar06("cc"), d};
+    cout << "------------  9  ----------------" << endl;
+    p3.reset(p.release());
+    cout << "------------  10  ----------------" << endl;
+  }
+  cout << "------------ 11  ----------------" << endl;
+
+  cout << "done." << endl;
+}
+
+void tmp06c() {
+  BarDeleter06 d{};
+
+  cout << "==================   1    ======================" << endl;
+  {
+    cout << "---------------------------" << endl;
+    std::unique_ptr<Bar06, BarDeleter06> p1{new Bar06("aa"), d};
+    cout << "---------------------------" << endl;
+    std::unique_ptr<Bar06, BarDeleter06> p2{std::move(p1)};
+    cout << "---------------------------" << endl;
+  }
+  cout << "===================================== \n\n" << endl;
+
+  cout << "==================   2    ======================" << endl;
+  {
+    cout << "---------------------------" << endl;
+    std::unique_ptr<Bar06, BarDeleter06&> p1{new Bar06("aa"), d};
+    cout << "---------------------------" << endl;
+    std::unique_ptr<Bar06, BarDeleter06&> p2{std::move(p1)};
+    cout << "---------------------------" << endl;
+  }
+  cout << "===================================== \n\n" << endl;
+
+  cout << "==================   3    ======================" << endl;
+  {
+    cout << "---------------------------" << endl;
+    std::unique_ptr<Bar06, BarDeleter06&> p1{new Bar06("aa"), d};
+    cout << "---------------------------" << endl;
+    std::unique_ptr<Bar06, BarDeleter06> p2{std::move(p1)};
+    cout << "---------------------------" << endl;
+  }
+  cout << "===================================== \n\n" << endl;
+
+  cout << "==================   4    ======================" << endl;
+  {
+    std::unique_ptr<Bar06, BarDeleter06&> p1{new Bar06("aa"), d};
+    cout << "a ---------------------------" << endl;
+    {
+      BarDeleter06 d1{};
+      d1.name = "one";
+      cout << "b ---------------------------" << endl;
+      std::unique_ptr<Bar06, BarDeleter06&> p2{new Bar06("bb"), d1};
+      cout << "c ---------------------------" << endl;
+      p1.swap(p2);
+      cout << "d ---------------------------" << endl;
+    }
+    cout << "e ---------------------------" << endl;
+  }
+  cout << "===================================== \n\n" << endl;
+
+  d.name = "NULL";
+
+  cout << "==================   5    ======================" << endl;
+  {
+    std::unique_ptr<Bar06, BarDeleter06> p1{new Bar06("aa"), d};
+    cout << "a ---------------------------" << endl;
+    {
+      BarDeleter06 d1{};
+      d1.name = "one";
+      cout << "b ---------------------------" << endl;
+      std::unique_ptr<Bar06, BarDeleter06> p2{new Bar06("bb"), d1};
+      cout << "c ---------------------------" << endl;
+      p1.swap(p2);
+      cout << "d ---------------------------" << endl;
+    }
+    cout << "e ---------------------------" << endl;
+  }
+  cout << "===================================== \n\n" << endl;
+
+  cout << "done." << endl;
 }
 
 }  // namespace tmp_test4
 
 void tmpTest4() {
   using namespace tmp_test4;
-  tmp03();
+  tmp06c();
 }
