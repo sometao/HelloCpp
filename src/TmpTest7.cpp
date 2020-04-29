47ea04b7d6d7e5c4d885bed3b3a535a6fa7d13d1
Tao Zhang <zta@outlook.com>
2020-04-29 14:04:13 +0800

    update

--- src/TmpTest7.cpp
+++ src/TmpTest7.cpp
@@ -1,4 +1,3 @@
-
 #include <cassert>
 #include <atomic>
 #include <fstream>
@@ -13,14 +12,14 @@
 #include <typeinfo>
 #include <unordered_map>
 #include <vector>
+#include <chrono>
 
-#include "EasyWay.h"
-#include "stdafx.h"
 
 namespace tmp_test7 {
 using std::cout;
 using std::endl;
 using std::string;
+using std::vector;
 
 template <typename T>
 void writeData(uint8_t* buf, T num) {
@@ -32,7 +31,7 @@ void writeData(uint8_t* buf, T num) {
 
 template <typename T>
 void readData(uint8_t* buf, T& num) {
-  int len(sizeof(T));
+  uint8_t len(sizeof(T));
   static uint8_t byMask(0xFF);
   num = 0;
 
@@ -105,7 +104,81 @@ void tmp01() {
 
   testWriteAndRead(vec4, data, 128);
 
+};
 
+int64_t currentTime() {
+  using namespace std::chrono;
+  auto time_now = system_clock::now();
+  auto durationIn = duration_cast<milliseconds>(time_now.time_since_epoch());
+  return durationIn.count();
+};
+
+class TMP2A{
+public :
+  const string name;
+  TMP2A(const string& n): name(n) {
+    cout << "TMP2A created with [" << name << "]" << endl;
+  }
+  void say() {
+    cout << "I am TMP2A with [" << name << "]" << endl;
+  }
+};
+
+
+class TMP2B{
+public :
+  const string alias;
+  TMP2B(const string& n): alias(n) {
+    cout << "TMP2B created with [" << alias << "]" << endl;
+  }
+  void say() {
+    cout << "I am TMP2B with [" << alias << "]" << endl;
+  }
+};
+
+template<typename T>
+void foo(T t) {
+  cout << "1. -----" << endl;
+  static T x{"Inner--" + std::to_string(currentTime())};
+  cout << "2. -----" << endl;
+  t.say();
+  cout << "3. -----" << endl;
+  x.say();
+  cout << "4. -----" << endl;
+}
+
+
+
+
+
+void tmp02() {
+
+  TMP2A a{"AAA"};
+  foo(a);
+  cout << "++++++++++++++++++++++++++++++++++++++\n\n" << endl;
+
+  TMP2B b{"BBB"};
+  foo(b);
+  cout << "++++++++++++++++++++++++++++++++++++++\n\n" << endl;
+
+  
+  TMP2A c{"CCC"};
+  foo(c);
+  foo(c);
+  foo(c);
+  cout << "++++++++++++++++++++++++++++++++++++++\n\n" << endl;
+
+  TMP2B d{"DDD"};
+  foo(d);
+  foo(d);
+  foo(d);
+  cout << "++++++++++++++++++++++++++++++++++++++\n\n" << endl;
+
+
+
+
+
+  cout << "done" << endl;
 
 
 }
@@ -114,5 +187,5 @@ void tmp01() {
 
 void tmpTest7() {
   using namespace tmp_test7;
-  tmp01();
+  tmp02();
 }
