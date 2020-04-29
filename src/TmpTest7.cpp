a406e2583765c6b1106f6d72f8b7f12a0c4bc626
Tao Zhang <zta@outlook.com>
2020-04-29 11:04:52 +0800

    update

--- src/TmpTest7.cpp
+++ src/TmpTest7.cpp
@@ -1,4 +1,5 @@
 
+#include <cassert>
 #include <atomic>
 #include <fstream>
 #include <functional>
@@ -8,6 +9,7 @@
 #include <regex>
 #include <sstream>
 #include <string>
+#include <cstring>
 #include <typeinfo>
 #include <unordered_map>
 #include <vector>
@@ -32,7 +34,6 @@ template <typename T>
 void readData(uint8_t* buf, T& num) {
   int len(sizeof(T));
   static uint8_t byMask(0xFF);
-  T temp(0);
   num = 0;
 
   for (size_t i = 0; i < len; ++i) {
@@ -41,45 +42,72 @@ void readData(uint8_t* buf, T& num) {
   }
 }
 
+template <typename T>
+void testWriteAndRead(vector<T> vec, uint8_t* data, size_t len) {
+
+  cout << "-------------------- "<< typeid(T).name() << " test ---------------------------" << endl;
+  memset(data, 0, len);
+  uint8_t width =sizeof(T);
+
+  cout << "width: " << (int)width << endl;
+  assert(len > width * vec.size());
+
+  for (uint8_t i = 0; i < vec.size(); i++) {
+    cout << "i:" << (int)i << ":" << typeid(vec[i]).name() << ":" << (int)vec[i] << endl;
+    uint16_t t = (uint16_t)i * width;
+    writeData(data + t, vec[i]);
+  }
+
+  cout << "--------------------" << endl;
+
+  for (int i = 0; i < len; i++) {
+    if(i % 8 == 0) {
+      cout << endl;
+      cout << "["<< i << "\t]\t";
+
+    }
+    cout << (int)data[i] << " ";
+  }
+
+  cout << endl;
+  cout << "--------------------" << endl;
+
+  for (uint8_t i = 0; i < vec.size(); i++) {
+    T d{0};
+    uint16_t t = (uint16_t)i * width;
+    readData(data + t, d);
+    cout << "read: i:" << (int)i << ":" << typeid(d).name() << ":" << (int)d << endl;
+  }
+
+
+  cout << "\n\n" << endl;
+
+}
+
+
 void tmp01() {
   cout << "tmpTest7 tmp01: call foo1." << endl;
-
-  // TODO write data to buffer;
-  // read data from buffer.
   uint8_t data[128] = {0};
-  vector<int> vec1{1, 2, 3, 4, 5, 6, 7};
 
-  //for (const auto& i : vec1) {
-  //  cout << "i:" << typeid(i).name() << ":" << i << endl;
-  //}
 
-  for (int i = 0; i < vec1.size(); i++) {
-    cout << "i:" << i << ":" << typeid(i).name() << ":" << vec1[i] << endl;
-  }
-  
-  for (uint8_t i = 0; i < vec1.size(); i++) {
-    uint16_t t = (uint16_t)i * 4;
-    writeData(data + t, vec1[i]);
-  }
+  vector<int> vec1{1024, 1025, 1026, 1027, 1028, 1029, 256};
+  vector<uint16_t> vec2{1024, 1025, 1026, 1027, 1028, 1029, 256};
+  vector<uint64_t> vec3{1024, 1025, 1026, 1027, 1028, 1029, 256};
 
-  //for (uint8_t i = 0; i < sizeof(data); i++) {
-  //  cout << "i:" << (int)i << ":" << typeid(data[i]).name() << ":" << (int)data[i] << endl;
-  //}
+  vector<uint8_t> vec4{2, 4, 6, 8, 16, 32, 64};
 
-  cout << "-----------------------------------------------" << endl;
 
-  for (uint8_t i = 0; i < vec1.size(); i++) {
-    int d{0};
-    uint16_t t = (uint16_t)i * 4;
-    readData(data + t, d);
-    cout << "read: i:" << (int)i << ":" << typeid(d).name() << ":" << d << endl;
+  testWriteAndRead(vec1, data, 128);
 
-  }
+  testWriteAndRead(vec2, data, 128);
+
+  testWriteAndRead(vec3, data, 128);
+
+  testWriteAndRead(vec4, data, 128);
 
 
 
 
-  // foo1();
 }
 
 }  // namespace tmp_test7
